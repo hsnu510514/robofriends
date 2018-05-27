77e0a884ba5f1340776bb3d9d0e54afcf5091814
Sean Wen <hsnu510514@gmail.com>
2018-05-27 14:20:48 +0800

    robofriends react

---
+++ src/Robot.js
@@ -0,0 +1,17 @@
+export const Robot = [
+{
+	id: 1,
+	name: 'Allen',
+	email: 'allen@gmail.com'
+} ,
+{
+	id: 2,
+	name: 'David',
+	email: 'david@gamil.com'
+},
+{
+	id: 3,
+	name: 'Chris',
+	email: 'chris@gmail.com'
+}
+]
